ea786f6fe09ee89b77aa124661e3ed74fefebb21
purnima <purnimasingh26th@gmail.com>
2023-08-10 14:06:53 +0530

    first commit

--- express_project/mycontact_backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getContacts ,CreateContact, getContact, updateContact, deleteContact} = require("../controllers/contactController"); 
-
-router.route("/").get (getContacts).post (CreateContact);
-
-router.route("/:id").get (getContact).put (updateContact).delete (deleteContact);
-
-module.exports = router;
